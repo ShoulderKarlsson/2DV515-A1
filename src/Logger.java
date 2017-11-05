e652e99eaf3787abcbf296c52382cd31e4efd8e3
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-11-05 14:52:55 +0100

    fix: changed logger and loggin a lot less

--- src/Logger.java
+++ src/Logger.java
@@ -1,30 +1,21 @@
 class Logger {
 
-    static void displayLevelProgress(int currentChunk, int total, int level) {
-        System.out.println("Storing chunk #" + currentChunk + " of " + total + " on " + "level " + level + ".");
-    }
-
-    static void displayLevelInformation(int levelAmount, int level) {
-        System.out.println();
-        System.out.println("Pages on level " + level + " is " + levelAmount);
-    }
-
     static void displayFinishedScrape(int totalAmount, String category) {
         System.out.println("Done with " + category);
         System.out.println("Saved " + totalAmount + " pages.");
     }
 
-    static void displayDoneWithLevelInformaiton(int amount, int level) {
-        System.out.println("Done with level " + level);
-        System.out.println("Have stored a total of " + amount + " pages so far.");
-    }
 
-    static void displayStartingScrape(String page, int chunkSize, int maxDepth) {
+    static void displayStartingScrape(String page) {
         System.out.println();
         System.out.println("=============================");
         System.out.println("Crawling from " + page);
-        System.out.println("Chunk size: " + chunkSize);
-        System.out.println("Max level depth: " + maxDepth);
         System.out.println("=============================");
+    }
+
+    static void displayChunkingProgress(int totalPages, int maxPages) {
+        int left = maxPages - totalPages;
+        System.out.println("Have currently stored " + totalPages + " out of " + maxPages
+                + ". (" + left + " pages left)");
     }
 }
